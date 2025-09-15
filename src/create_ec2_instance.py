afde860034a84132a7b8127716ec075178cb60a7
Roman Roman <romanroman2705@gmail.com>
2025-09-15 11:17:39 -0400

    add: group waiter to wait for security group to be created before creating instance

--- src/create_ec2_instance.py
+++ src/create_ec2_instance.py
@@ -1,64 +1,87 @@
 import boto3
 
-ec2 = boto3.resource("ec2", region_name="us-east-1")  # change region if needed
+# Resource is a high-level API
+# Client is a low-level API
+ec2_resource = boto3.resource("ec2", region_name="us-east-1")
+ec2_client = boto3.client("ec2", region_name="us-east-1")
+sg_name = "lab01-security-group"
+
+def security_group_exists(sg_name):
+    exists = False
+    try:
+        response = ec2_client.describe_security_groups(
+            Filters=[{"Name": "group-name", "Values": [sg_name]}]
+        )
+        if len(response["SecurityGroups"]) != 0:
+            exists = True
+    except Exception as e:
+        print("Error while checking if security group exists: ", e)
+    
+    return exists
+
 
 def create_security_group():
-    sg_name = "lab01-security-group"
     vpc_id = "vpc-08afba995b983b2c4"
     
     # Check if the security group already already exists before creating it
-    try:
-        for sg in ec2.security_groups.all():
-            if sg.group_name == sg_name:
-                return
-    except Exception as e:
-        print("Error while checking SG:", e)
+    if security_group_exists(sg_name):
+        return
 
     # Create security group  
-    response = ec2.create_security_group(
+    security_group = ec2_client.create_security_group(
         GroupName=sg_name,
         Description="Security group for lab01 in LOG8415E",
         VpcId=vpc_id  # replace with your default VPC ID
     )
+    print("Security group creation started. Security group id: ", security_group["GroupId"])
 
-    security_group_id = response["GroupId"]
-    print("Created security group:", security_group_id)
+    # Wait for security group to be created before adding rules
+    group_waiter = ec2_client.get_waiter('security_group_exists')
+    group_waiter.wait(GroupNames=[sg_name])
+    print("Security group creation finished")
 
     # Add inbound rules
-    ec2.authorize_security_group_ingress(
-        GroupId=security_group_id,
+    ec2_client.authorize_security_group_ingress(
+        GroupName=sg_name,
+        GroupId=security_group["GroupId"],
         IpPermissions=[
             {
                 "IpProtocol": "tcp",
                 "FromPort": 22,
                 "ToPort": 22,
-                "IpRanges": [{"CidrIp": "0.0.0.0/0"}]  # or better: just your IP
+                "IpRanges": [{"CidrIp": "0.0.0.0/0"}]
             },
             {
                 "IpProtocol": "tcp",
                 "FromPort": 8000,
                 "ToPort": 8000,
                 "IpRanges": [{"CidrIp": "0.0.0.0/0"}]
-            }
+            },
+            {
+                'FromPort': 80,
+                'ToPort': 80,
+                'IpProtocol': 'tcp',
+                'IpRanges': [{'CidrIp': '0.0.0.0/0'}]
+            },
         ]
     )
 
 
 def create_instance():
-    instances = ec2.create_instances(
+    instances = ec2_resource.create_instances(
         ImageId="ami-00ca32bbc84273381",   # Amazon Linux 2 AMI (for us-east-1, update if in another region)
         MinCount=1,
         MaxCount=1,
         InstanceType="t2.micro",          # or t2.large for your lab
         KeyName="LOG8415_key",             # replace with your key pair name
-        SecurityGroupIds=["lab01-security-group"]
+        SecurityGroupIds=[sg_name],
         # SecurityGroupIds=["sg-xxxxxxxx"], # replace with a Security Group that allows SSH (port 22) + HTTP (port 8000)
-        # TagSpecifications=[
-        #     {
-        #         "ResourceType": "instance",
-        #         "Tags": [{"Key": "Name", "Value": "MyLabInstance"}]
-        #     }
-        # ]
+        TagSpecifications=[
+            {
+                "ResourceType": "instance",
+                "Tags": [{"Key": "Lab", "Value": "LAB01"}]
+            }
+        ]
     )
 
     instance = instances[0]

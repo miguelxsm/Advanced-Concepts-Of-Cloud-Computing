ea78d5eae52359c9ddf657cac2906207419a1be4
miguelxsm <mcarrascoguirao@gmail.com>
2025-09-20 14:25:25 -0400

    Modify: .gitignore for .venv/

--- user_data/cluster1.sh
+++ user_data/cluster1.sh
@@ -20,3 +20,31 @@ export CLUSTER_NAME=cluster1
 # ===== 5) Deploy =====
 cd /home/ec2-user/app/app
 nohup python3 -m uvicorn main:app --host 0.0.0.0 --port 8000 >/home/ec2-user/app.log 2>&1 &
+
+#!/bin/bash
+set -e
+# ===== Base packages =====
+apt-get update -y
+apt-get install -y python3 python3-pip git
+
+
+"""
+Instead of manually transferring the FastAPI app with scp, we automated the process by providing a user-data script that installs dependencies and fetches the app from a GitHub repository. This way, each instance is ready to serve requests immediately after launch.
+"""
+# ===== 2) Clone repository =====
+# Sustituye la URL por la tuya:
+cd /home/ubuntu
+if [ ! -d app ]; then
+  git clone https://github.com/miguelxsm/Advanced-Concepts-Of-Cloud-Computing.git app
+fi
+
+# ===== Dependencies =====
+pip3 install -r app/requirements.txt
+
+# ===== 4) Defining Cluster =====
+echo 'export CLUSTER_NAME=cluster1' > /etc/profile.d/cluster.sh # explain in the report how this helps
+export CLUSTER_NAME=cluster1
+
+# ===== 5) Deploy =====
+cd /home/ubuntu/app
+nohup python3 -m uvicorn main:app --host 0.0.0.0 --port 8000 >/home/ubuntu/app.log 2>&1 &
